917fe224bf7263e54765717006949b89d3b915dd
galubezzz <galubezzz@gmail.com>
2019-02-19 23:22:23 -0800

    test loading joke into the state

--- src/App.js
+++ src/App.js
@@ -1,28 +1,32 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    state = {
+        joke: {}
+    };
+
+    componentDidMount() {
+        const P_URL = 'https://api.chucknorris.io/jokes/random';
+        fetch(P_URL).then(response => {
+            if (response.ok) {
+                return response.json();
+            }
+            throw new Error('Something went wrong with network request');
+        }).then(posts => {
+            this.setState({joke: posts});
+        }).catch(error => {
+            console.log(error);
+        });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                {this.state.joke.value}
+            </div>
+        );
+    }
 }
 
 export default App;
